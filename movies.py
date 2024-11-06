af0559d8eb8d5a13a3f52394df8a6d2754d5bbad
derikluc <dluc@terpmail.umd.edu>
2024-11-05 21:42:00 -0500

    finalized movies.py
    added docstrings

--- movies.py
+++ movies.py
@@ -1,17 +1,33 @@
-"""Identify the most popular movie ratings based on data in two CSV files."""
+"""Identify the most popular movie ratings based on data in two CSV files.
+Driver: Derik Luc
+Navigator: Matthew Zou
+Assignment: Exercise 9 Movie Ratings
+Date: 11/5/2024
+"""
 
 from argparse import ArgumentParser
 import pandas as pd
 import sys
 
-
 def best_movies(movies, ratings):
+    """
+    Sorts the movies by best rated to worst rated by merging 2 data files
+    
+    Args:
+    movies and ratings are both CSV files containing data
+    
+    Returns:
+    Sorted movie date from highest to lowest rated
+    """
+    
     movie = pd.read_csv(movies)
     rating = pd.read_csv(ratings)
 
-    inner_merge = pd.merge(movie, rating, left='movie id',right='item id', how='inner')
+    # inner_merge = pd.merge(movie, rating, left='movie id',right='item id', how='inner')
 
-    average_ratings = inner_merge.groupby('title')['rating'].mean()
+    inner_merge = movie.merge(rating, how = 'inner', left_on = "movie id", right_on = "item id")
+
+    average_ratings = inner_merge.groupby('movie title')['rating'].mean()
 
     return average_ratings.sort_values(ascending=False)
 
